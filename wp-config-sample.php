070e61b792d7b10542fbf48d14e2e0deb91634d9
Metin Ucar <mtn.ucar@gmail.com>
2015-05-16 22:17:44 +0300

    Adds a customized wp-config.php

---
+++ wp-config-sample.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'database_name_here');
+
+/** MySQL database username */
+define('DB_USER', 'username_here');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password_here');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+# Enable all core updates, including minor and major:
+define ( ‘WP_AUTO_UPDATE_CORE’, true );
+
+# Skip wp-content directory while updating
+define( 'CORE_UPGRADE_SKIP_NEW_BUNDLED', true );
+
+# Allowing unfiltered uploads (admins only)
+define( 'ALLOW_UNFILTERED_UPLOADS', true );
+
+# Enable Trash feature for media files
+define( 'MEDIA_TRASH', true );
+
+# Define wordpress api key (for akismet and jetpack)
+define( 'WPCOM_API_KEY', 'YourKeyHere' );
+
+# Define default theme
+define( 'WP_DEFAULT_THEME', 'reen' );
+
+# Define the environment for the theme
+define('WP_ENV', 'production'); // production or development
+
+# Disable file edits
+define( 'DISALLOW_FILE_EDIT', true );
+
+# Disable plugin and theme changes
+define( 'DISALLOW_FILE_MODS', true );
+
+# Disable PHP error reporting
+error_reporting (0);
+@ini_set (‘display_errors’, 0);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
